8247f095ece69d1d67fa7aee455ff81dbeb87189
Ian <iaarsenault@email.neit.edu>
2016-09-03 00:04:07 -0400

    Added: order checkout and order complete page

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require ("models/dbConn.php");
 include ("views/header.php");
 include ("views/main.php");
 include ("views/footer.php");
-
+include ("views/checkout.php");
+include ("views/complete.php");
 
 ?>
